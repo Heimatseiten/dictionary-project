ab720e5f229cc7f9394e084aff585890571a4ce1
Tine Lüttgen <christine.luettgen@gmail.com>
2025-01-18 21:08:12 +0100

    update

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Logo from "./Logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={Logo} className="App-logo img-fluid" alt="logo" />
+      </header>
+      HELLO
+    </div>
+  );
+}
